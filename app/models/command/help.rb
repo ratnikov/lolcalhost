20fbaf2c2b1edda7c3a89f168868b93fe8264f9c
Dmitry Ratnikov <ratnikov@gmail.com>
2010-03-04 00:03:46 -0600

    Added display of help for specific commands.

--- app/models/command/help.rb
+++ app/models/command/help.rb
@@ -1,5 +1,31 @@
 class Command::Help < Command::External
+  help "Shows help", :usage => "help [ command ]"
+
   def output
+    if command = args.first
+      command_help(command)
+    else
+      general_help
+    end
+  end
+
+  private
+
+  def general_help
     "Try typing in one of the following commands:\n#{Command.commands.sort.map { |name| '  ' + name }.join("\n")}"
+  end
+
+  def command_help(command)
+    if command_klass = Command.command_space[command]
+      output = "Help for command #{command_klass.command_name.inspect}:\n"
+
+      manual = command_klass.manual
+      output += "\nUsage: #{manual.usage}" unless manual.usage.nil?
+
+      output += "\nDescription:\n\n#{manual.description}"
+      output
+    else
+      Command::Unknown.new(:command => command).output
+    end
   end
 end
--- test/unit/command/help_test.rb
+++ test/unit/command/help_test.rb
@@ -3,14 +3,45 @@ require 'test_helper'
 class Command::HelpTest < ActiveSupport::TestCase
   should("pass truthitest") { assert true }
 
-  should("have #output return all current commands sorted by name") do
-    assert_match Regexp.new(Command.commands.sort.join('.*'), Regexp::MULTILINE), Command::Help.new.output
+  context "with no arguments" do
+    should("have #output return all current commands sorted by name") do
+      assert_match Regexp.new(Command.commands.sort.join('.*'), Regexp::MULTILINE), Command::Help.new.output
+    end
+
+    should("not include internal commands") do
+      klass = Class.new(Command::Internal)
+      def klass.name; 'InternalCommand' end
+
+      assert_no_match /internal/i, Command::Help.new.output
+    end
   end
 
-  should("include internal commands") do
-    klass = Class.new(Command::Internal)
-    def klass.name; 'InternalCommand' end
+  context "with a command argument" do
+    setup do
+      @klass = Class.new(Command::External) do |klass|
+        def klass.name; to_s end
+      end
 
-    assert_no_match /internal/i, Command::Help.new.output
+      @klass.help 'Some help message', :usage => 'be used in some way'
+      @output = Command::Help.new(:args => [ @klass.command_name ]).output
+    end
+
+    should("mention the name of the command in output") do
+      assert_match @klass.command_name, @output
+    end
+
+    should("mention the description of the command in output") do
+      assert_match /Some help message/, @output
+    end
+
+    should("mention the usage") do
+      assert_match /be used in some way/, @output
+    end
+  end
+
+  context "with an unknown command argument" do
+    should("return unknown command's output") do
+      assert_equal Command::Unknown.new(:command => 'some_command').output, Command::Help.new(:args => [ 'some_command' ]).output
+    end
   end
 end
